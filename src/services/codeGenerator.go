d738c4c5ebe8eaef6e30146b94d52de4fba9a118
HarbyTorres <habytorres29@gmail.com>
2022-04-21 14:08:51 -0500

    code python generator

--- src/services/codeGenerator.go
+++ src/services/codeGenerator.go
@@ -7,22 +7,53 @@ import (
 	"strconv"
 )
 
+type CodeGenerator2 struct {
+}
+
 func CodeGenerator(draw models.DrawflowMap) string {
 	var nodes []models.Node
-	nodes = processDrawflow(draw)
+	nodes = processDrawflowMap(draw)
+
+	res := "def func():\n"
 
 	for i := 0; i < len(nodes); i++ {
 		switch nodes[i].Class {
 		case "number":
-			fmt.Println("number")
-		case "add":
-			fmt.Println("add")
+			res += numCodeGenerator(nodes[i])
+			break
+		case "if":
+			fmt.Println("if")
+			break
+		default:
+			res += oppCodeGenerator(nodes[i])
 		}
 	}
-	return "Ok"
+	fmt.Println(res)
+	return res
 }
 
-func processDrawflow(draw models.DrawflowMap) []models.Node {
+func oppCodeGenerator(node models.Node) string {
+	num1 := node.Inputs.Input1.Connections[0].Node
+	num2 := node.Inputs.Input2.Connections[0].Node
+	var opp string
+	switch node.Class {
+	case "add":
+		opp = "+"
+	case "subt":
+		opp = "-"
+	case "divide":
+		opp = "/"
+	case "multiply":
+		opp = "*"
+	}
+	return fmt.Sprintf("\tnum%v = num%v %s num%v\n", node.ID, num1, opp, num2)
+}
+
+func numCodeGenerator(node models.Node) string {
+	return fmt.Sprintf("\tnum%v = %v\n", node.ID, node.Data["num"])
+}
+
+func processDrawflowMap(draw models.DrawflowMap) []models.Node {
 	var nodes []models.Node
 	for i := 1; i <= len(draw.Drawflow.Home.Data); i++ {
 		var node models.Node
