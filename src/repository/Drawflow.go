eebcb621b24787b0075738c16a70a337fdc430c0
HarbyTorres <habytorres29@gmail.com>
2022-04-27 09:32:17 -0500

    save drawflow repository

--- src/repository/Drawflow.go
+++ src/repository/Drawflow.go
@@ -2,7 +2,10 @@ package repository
 
 import (
 	"apinodos/src/models"
+	"encoding/json"
+	"fmt"
 	"strings"
+	"time"
 )
 
 type DrawflowMuttations struct {
@@ -10,14 +13,31 @@ type DrawflowMuttations struct {
 
 func (d *DrawflowMuttations) SaveDrawflow(draw models.CreateDrawflow) *strings.Reader {
 
-	payload := strings.NewReader("{\"query\":\"mutation MyMutation {\\r\\n  addDrawflow(input: {body: \\\"drawflowtest3\\\", crateDate: \\\"2022-04-22\\\", name: \\\"drawformback2\\\"}) {\\r\\n    numUids\\r\\n  }\\r\\n}\",\"variables\":{}}")
+	jsonbody, err := json.Marshal(draw.Body.Drawflow.Home.Data)
+	if err != nil {
+		panic(err)
+	}
+
+	t := time.Now()
+	date := fmt.Sprintf("%d-%02d-%02d",
+		t.Year(), t.Month(), t.Day(),
+	)
+
+	data := fmt.Sprintf("{\"query\":\"mutation MyMutation {\\r\\n  addDrawflow(input: {body: \\\"%v\\\", createdAt: \\\"%v\\\", name: \\\"%v\\\"}) {\\r\\n    numUids\\r\\n  }\\r\\n}\",\"variables\":{}}",
+		jsonbody,
+		date,
+		draw.Name,
+	)
+	fmt.Println(data)
+
+	payload := strings.NewReader(data)
 
 	return payload
 }
 
 func (d *DrawflowMuttations) GetDrawflows() *strings.Reader {
 
-	payload := strings.NewReader("{\"query\":\"query MyQuery {\\r\\n  queryDrawflow {\\r\\n    body\\r\\n    crateDate\\r\\n    id\\r\\n    name\\r\\n  }\\r\\n}\\r\\n\",\"variables\":{}}")
+	payload := strings.NewReader("{\"query\":\"query MyQuery {\\r\\n  queryDrawflow {\\r\\n    body\\r\\n    createdAt\\r\\n    id\\r\\n    name\\r\\n  }\\r\\n}\\r\\n\",\"variables\":{}}")
 
 	return payload
 }
